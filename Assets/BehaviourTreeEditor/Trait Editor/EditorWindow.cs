f36dd90d546a45b472e1170cef16e3ac4722426e
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2021-01-06 22:38:27 +0100

    Initial commit

---
+++ Assets/BehaviourTreeEditor/Trait Editor/EditorWindow.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class EditorWindowExample : EditorWindow
+{
+    // Create new trait that will be modified
+    static Trait trait;
+    public static Trait NewTrait { get { return trait; } }
+
+    void OnEnable()
+    {
+        InitData();
+    }
+
+    public static void InitData()
+    {
+        trait = (Trait)ScriptableObject.CreateInstance(typeof(Trait));
+    }
+
+    [MenuItem("Window/Trait Editor")]
+    public static void OpenWindow()
+    {
+        EditorWindowExample window = (EditorWindowExample)GetWindow(typeof(EditorWindowExample));
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Trait name");
+        //Edit trait name
+        trait.traitName = EditorGUILayout.TextField(trait.traitName);
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Save trait"))
+        {
+            SaveTraitData();
+        }
+    }
+
+
+    void SaveTraitData()
+    {
+        string traitPath = "Assets/Prefabs/traits/" + trait.traitName;
+        // create the .asset file
+        Trait newTrait = (Trait)ScriptableObject.CreateInstance(typeof(Trait));
+        newTrait.traitName = trait.traitName;
+        AssetDatabase.CreateAsset(newTrait, traitPath + ".asset");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Recources/Tool/Trait.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Trait : ScriptableObject
+{
+    public string traitName;
+}
